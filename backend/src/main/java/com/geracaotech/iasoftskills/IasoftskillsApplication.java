39ac6f2cfde6899b0368315041c5ecf1304dd127
Alisson Patricio Gomes de Freitas <alissonpatriciodefreitas@gmail.com>
2026-03-25 11:20:45 -0300

    projeto inicial

---
+++ backend/src/main/java/com/geracaotech/iasoftskills/IasoftskillsApplication.java
@@ -0,0 +1,13 @@
+package com.geracaotech.iasoftskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasoftskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IasoftskillsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/geracaotech/iasoftskills/IasoftskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geracaotech.iasoftskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasoftskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
