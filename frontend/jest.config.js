eba80806b8ecb330b8b9411b3f93d04b71e10963
Alisson Patricio Gomes de Freitas <alissonpatriciodefreitas@gmail.com>
2026-03-31 13:29:20 -0300

    fix: remover babel custo e usar next/jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-  },
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
+
+module.exports = createJestConfig(customJestConfig);
